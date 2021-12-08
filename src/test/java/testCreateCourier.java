8abbca7d8ecb0942f9ce293ac00c371c0cbe7e3c
buyan.fred@gmail.com <buyan.fred@gmail.com>
2021-12-08 11:53:31 +0300

    оформил создание курьера

--- src/test/java/testCreateCourier.java
+++ src/test/java/testCreateCourier.java
@@ -1,4 +1,3 @@
-import com.example.scooterRegisterCourier;
 import io.restassured.response.Response;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Before;
@@ -12,7 +11,10 @@ import static org.junit.Assert.assertNotEquals;
 
 public class testCreateCourier {
     ArrayList<String> loginPass = new ArrayList<>();
-    String l,p,fn,hint;
+    String err409conflict ="{\"code\":400,\"message\":\"Недостаточно данных для создания учетной записи\"}";
+    String code200ok ="{\"ok\":true}";
+    String l,p,fn, answerBody;
+    int code;
 
     int create(String l, String p, String f) {
         String registerRequestBody = "{\"login\":\"" + l + "\","
@@ -24,23 +26,47 @@ public class testCreateCourier {
                 .body(registerRequestBody)
                 .when()
                 .post("https://qa-scooter.praktikum-services.ru/api/v1/courier");
-        hint = response.getBody().asString();
-        System.err.println(hint);
-        return response.statusCode();
+        answerBody = response.getBody().asString();
+        code = response.statusCode();
+        System.err.println(answerBody +','+ code);
+        return  code;
+    }
+    int login(String l, String p){
+        String registerRequestBody = "{\"login\":\"" + l + "\","
+                + "\"password\":\"" + p + "\"}";
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(registerRequestBody)
+                .when()
+                .post("https://qa-scooter.praktikum-services.ru/api/v1/courier/login");
+        answerBody =response.getBody().asString();
+        code =response.statusCode();
+        System.err.println(answerBody + code);
+        return code;
     }
 
     @Before
     public void before(){
        l = RandomStringUtils.randomAlphabetic(10);
         // с помощью библиотеки RandomStringUtils генерируем пароль
-         p = RandomStringUtils.randomAlphabetic(10);
+       p = RandomStringUtils.randomAlphabetic(10);
         // с помощью библиотеки RandomStringUtils генерируем имя курьера
-         fn = RandomStringUtils.randomAlphabetic(10);
+       fn = RandomStringUtils.randomAlphabetic(10);
+       answerBody ="";
+       code=0;
+    }
+    @Test
+    public void testCreationCourierIsPossible(){
+        create(l,p,fn);
+        assertEquals("Creation courier",
+                code200ok,
+                answerBody
+        );
+        assertEquals("Login new courier successfull",200,login(l,p));
     }
      @Test
-    public void testOneCourierCannotCreateTwice(){
-
-
+    public void testSameCourierCannotCreateTwice(){
          assertNotEquals("One user created twice",
                  create(l,p,fn),
                  create(l,p,fn)
@@ -49,38 +75,20 @@ public class testCreateCourier {
      @Test
     public void testCannotCreateEmptyCourier(){
         assertEquals("Creation empty courier",400,create("","",""));
-    }
+        assertEquals(err409conflict,answerBody);
+     }
      @Test
     public void testCannotCreateCourierWithoutLogin(){
-         String l = RandomStringUtils.randomAlphabetic(10);
-         // с помощью библиотеки RandomStringUtils генерируем пароль
-         String p = RandomStringUtils.randomAlphabetic(10);
-         // с помощью библиотеки RandomStringUtils генерируем имя курьера
-         String fn = RandomStringUtils.randomAlphabetic(10);
-         //System.out.println(create("",p,fn));
          assertEquals("Creation courier without login",400,create("",p,fn));
-
+         assertEquals(err409conflict,answerBody);
      }
     @Test
     public void testCannotCreateCourierWithoutPassword(){
-        String l = RandomStringUtils.randomAlphabetic(10);
-        // с помощью библиотеки RandomStringUtils генерируем пароль
-        String p = RandomStringUtils.randomAlphabetic(10);
-        // с помощью библиотеки RandomStringUtils генерируем имя курьера
-        String fn = RandomStringUtils.randomAlphabetic(10);
-        //System.out.println(create("",p,fn));
         assertEquals("Creation courier without login",400,create(l,"",fn));
-
+        assertEquals(err409conflict,answerBody);
     }
     @Test
     public void testCorrectCreate(){
-        String l = RandomStringUtils.randomAlphabetic(10);
-        // с помощью библиотеки RandomStringUtils генерируем пароль
-        String p = RandomStringUtils.randomAlphabetic(10);
-        // с помощью библиотеки RandomStringUtils генерируем имя курьера
-        String fn = RandomStringUtils.randomAlphabetic(10);
-        //System.out.println(create(l,p,fn));
         assertEquals("Correct creation courier",201,create(l,p,fn));
-        assertEquals("{\"ok\":true}",hint);
     }
 }
